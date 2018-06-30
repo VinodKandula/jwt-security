157b91a4781970445bcdd0965e8618e7ddabc463
Zoltan Altfatter <zoltan.altfatter@mimacom.com>
2018-06-30 17:10:51 +0200

    initial commit

---
+++ jwt-security-core/src/main/java/com/example/jwtsecuritycore/JwtSecurityCoreApplication.java
@@ -0,0 +1,12 @@
+package com.example.jwtsecuritycore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecurityCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtSecurityCoreApplication.class, args);
+    }
+}
---
+++ jwt-security-starter/src/main/java/com/example/jwtsecuritystarter/JwtSecurityStarterApplication.java
@@ -0,0 +1,12 @@
+package com.example.jwtsecuritystarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecurityStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtSecurityStarterApplication.class, args);
+    }
+}
